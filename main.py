6610012e43d77f77bcd0b8e4d021446b60bc8c9c
Fredrik Mile <fredrik.mile@gmail.com>
2022-03-23 07:50:43 +0100

    Add initial script

---
+++ main.py
@@ -0,0 +1,18 @@
+import json
+from datetime import datetime
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+games = data['draws'][0]['drawEvents']
+
+
+
+print(data['draws'][0]['drawComment'])
+print("")
+for i, game in enumerate(games):
+    desc = game['eventDescription']
+    start = ":".join(game['match']['matchStart'].split('T')[1].split(":")[:2])
+    print(f"{i + 1}. {desc}".ljust(25) + f":: {start}")
+
+
