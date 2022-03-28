cc8c175ac47d208fc845cee9227957ad9f0e2130
Fredrik Mile <fredrik.mile@gmail.com>
2022-03-28 08:39:19 +0200

    Refactor and documented stryket_scraper.py

--- stryket_scraper.py
+++ stryket_scraper.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
 import re
 
 from bs4 import BeautifulSoup
 from pprint import pprint
 from requests_html import HTMLSession
 from enum import Enum
+
+URL = "https://spela.svenskaspel.se/resultat/stryktipset"
+GAME_DIVS_CLASS = "js-expandable-box"
 
 
 class GameStatus(Enum):
@@ -12,94 +16,144 @@ class GameStatus(Enum):
     not_started = 3
 
 
-URL = "https://spela.svenskaspel.se/resultat/stryktipset"
+def get_teams(team_names: list(str)) -> tuple(str, str):
+    """Returns the home team and the away team from a raw list of team names.
 
-session = HTMLSession()
-r = session.get(URL)
-r.html.render()
-soup = BeautifulSoup(r.html.raw_html, "html.parser")
+    Parameters
+    __________
+    team_names : list(str)
+        The raw list of team names. The '-' is used as team delimiter.
+        Example:
+            ["Port", "Vale", "-", "Lincon"] or ["Arsenal", "-", "Manchester", "United"]
 
-game_divs = soup.find_all("div", {"class": "js-expandable-box"})
+    Returns
+    _______
+    tuple(str, str)
+        A tuple with home team as first element and away team as last element.
+        Example:
+            ("PortVale", "Licon") or ("Arsenal", "ManchesterUnited")
 
-games = []
-
-for i, div in enumerate(game_divs):
-    raw_info = div.get_text().replace("\n", " ").replace("\r", "")
-    info_parts = raw_info.split()
-    try:
-        # All interesting rows starts with a number between 1 and 13.
-        int(info_parts[0])
-    except ValueError:
-        break
-
-    status = ""
-    rev_info_parts = list(reversed(info_parts))
-    try:
-        # If the game has started, the last item is the score of the away team
-        # If the game has not started, the last item is the start time. Eg 18:00.
-        int(rev_info_parts[0])
-        status = GameStatus.live
-    except ValueError:
-        if ":" in rev_info_parts[0]:
-            status = GameStaus.not_started
+    """
+    home_team = ""
+    away_team = ""
+    home_team_done = False
+    for item in team_names:
+        if item == "-":
+            home_team_done = True
+        elif not home_team_done:
+            home_team += item
         else:
-            status = GameStatus.finished
+            away_team += item
+    return (home_team, away_team)
 
-    game = {}
-    game["status"] = status.name
-    game["number"] = i + 1
-    if status == GameStatus.live:
-        goals_away = rev_info_parts[0]
-        goals_home = rev_info_parts[2]
-        time = rev_info_parts[4]
-        team_names = list(reversed(rev_info_parts[5:-1]))
-        home_team = ""
-        away_team = ""
-        home_team_done = False
-        for item in team_names:
-            if item == "-":
-                home_team_done = True
-            elif not home_team_done:
-                home_team += item
+
+def fetch_raw_data() -> BeautifulSoup:
+    """Fetch the raw HTML and JS generated raw data from svenskaspel.
+
+    Returns
+    _______
+    BeautifulSoup: Soup object with the fully generated page. HTML and JS content.
+    """
+    session = HTMLSession()
+    r = session.get(URL)
+    r.html.render()
+    return BeautifulSoup(r.html.raw_html, "html.parser")
+
+
+def extract_game_info(soup: BeautifulSoup) -> list(dict):
+    """Extract game information from the soup representation of svenskaspels webpage.
+
+    Parameters
+    __________
+    soup : BeautifulSoup
+        Raw soup representation of the svenskaspels webpage.
+
+    Returns
+    _______
+    list(dict)
+        A list (of len 13) of dict objects representing a game.
+        A game is represented by a dict, the keys and values depend
+        on the status of the game. The status can be one of
+        'GameStatus.live', 'GameStatus.not_started' or 'GameStatus.finished'
+
+        All games have the following keys:
+            - status
+            - number
+            - home_team
+            - away_team
+        A game with 'GameStatus.live' have the following key/s:
+            - goals_home
+            - goals_away
+            - time
+        A game with 'GameStatus.not_started have the following extra key/s:
+            - start_time
+        A game with 'GameStatus.finished' have the following extra key/s:
+            - goals_home
+            - goals_away
+    """
+    game_divs = soup.find_all("div", {"class": GAME_DIVS_CLASS})
+
+    games = []
+    for i, div in enumerate(game_divs):
+        raw_info = div.get_text().replace("\n", " ").replace("\r", "")
+        info_parts = raw_info.split()
+        try:
+            # All game rows starts with a number between 1 and 13.
+            int(info_parts[0])
+        except ValueError:
+            break
+
+        status = ""
+        rev_info_parts = list(reversed(info_parts))
+        try:
+            # If the game has started, the last item is the score of the away team
+            int(rev_info_parts[0])
+            status = GameStatus.live
+        except ValueError:
+            # If the game has not started, the last item is the start time. Eg 18:00.
+            if ":" in rev_info_parts[0]:
+                status = GameStaus.not_started
             else:
-                away_team += item
-        game["goals_home"] = goals_home
-        game["goals_away"] = goals_away
-        game["time"] = time
-    elif status == GameStatus.not_started:
-        start_time = rev_info_parts[1] + " " + rev_info_parts[0]
-        team_names = list(reversed(rev_info_parts[2:-1]))
-        home_team = ""
-        away_team = ""
-        home_team_done = False
-        for item in team_names:
-            if item == "-":
-                home_team_done = True
-            elif not home_team_done:
-                home_team += item
-            else:
-                away_team += item
-        game["start_time"] = start_time
-    elif status == GameStatus.finished:
-        goals_home = info_parts[1]
-        goals_away = info_parts[3]
-        home_team = ""
-        away_team = ""
-        team_names = info_parts[5:]
-        home_team_done = False
-        for item in team_names:
-            if item == "-":
-                home_team_done = True
-            elif not home_team_done:
-                home_team += item
-            else:
-                away_team += item
-        game["goals_home"] = goals_home
-        game["goals_away"] = goals_away
+                status = GameStatus.finished
 
-    game["home_team"] = home_team
-    game["away_team"] = away_team
+        game = {}
+        game["status"] = status.name
+        game["number"] = i + 1
+        if status == GameStatus.live:
+            home_team, away_team = get_team(rev_info_parts[5:-1])
+            game["goals_home"] = rev_info_parts[2]
+            game["goals_away"] = rev_info_parts[0]
+            game["time"] = rev_info_parts[4]
+        elif status == GameStatus.not_started:
+            home_team, away_team = get_teams(info_parts[2:-1])
+            game["start_time"] = rev_info_parts[1] + " " + rev_info_parts[0]
+        elif status == GameStatus.finished:
+            home_team, away_team = get_teams(info_parts[5:])
+            game["goals_home"] = info_parts[1]
+            game["goals_away"] = info_parts[3]
 
-    games.append(game)
+        game["home_team"] = home_team
+        game["away_team"] = away_team
 
-pprint(games)
+        games.append(game)
+    return games
+
+
+def get_game_information() -> list(dict):
+    """Entry point for this module.
+    Fetch raw data and return a user friendly dict represenation of each one of the 13 games.
+
+    Returns
+    ______
+
+    list(dict)
+        List containing all 13 games represented as dict
+        Returns
+        ______
+
+        list(dict)
+            List containing all 13 games represented as dicts..
+    """
+    soup = fetch_raw_data()
+    return extract_game_info(soup)
+
